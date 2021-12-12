89661727b9e1ab291915eb6860162b84234cf923
root <yoshuaflores@ciencias.unam.mx>
2021-12-12 23:29:11 +0000

    mas cambios

--- Practica07/Tecnico.java
+++ Practica07/Tecnico.java
@@ -1,12 +1,11 @@
-public class Oficial extends Operador{
-    public Oficial(){
+public class Tecnico extends Operador{
+    public Tecnico(){
 	super();
     }
     @Override
     public String toString(){
-	return "Tecnico" + this.name;
+	return "Tecnico: " + this.name;
     }
-
     public String trabaja(){
 	return "Estoy trabajando";
     }
