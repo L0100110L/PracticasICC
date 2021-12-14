ce607d521a9fb0288c647166836ff3f48e4ab34b
Yoshua <Yoshua>
2021-12-13 20:55:17 -0600

    Detalles

--- Practica07/Herencia.java
+++
@@ -1,7 +0,0 @@
-public class Herencia extends Padre{
-    private String carrera;
-    public Herencia (String nombre, String apellido, String caracter, String carrera){
-	super(nombre, apellido, caracter);//Inicializar las variables de la clase que hereda
-	carrera = this.carrera
-    }
-}
