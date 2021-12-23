c35a691e745e7110550298cff4094fb13eaf045d
Yoshua <Yoshua>
2021-12-23 10:53:50 -0600

    Examen de diciembre

---
+++ Examenes/Salon.java
@@ -0,0 +1,49 @@
+/**
+ * Clase Salon. Muestra a los alumnos de un salón de clases.
+ *
+ */
+
+public class Salon {
+
+    public static void main(String [] argumentos) {
+
+	String [][] asientos ;
+
+	// asientos representa los lugares que ocupan los alumnos en el salón
+	asientos = new String[4][5] ;
+
+	// Los alumnos se acomodan así:
+	asientos[0][0] = "María" ;
+	asientos[0][1] = "Jorge" ;
+	asientos[0][2] = "Eric" ;
+	asientos[0][3] = "Valeria" ;
+	asientos[0][4] = "Ana" ;
+	asientos[1][0] = "Laura" ;
+	asientos[1][1] = "José" ;
+	asientos[1][2] = "Mónica" ;
+	asientos[1][3] = "Andrea" ;
+	asientos[1][4] = "Fernando" ;
+	asientos[2][0] = "Miguel" ;
+	asientos[2][1] = "Elizabeth" ;
+	asientos[2][2] = "Yonatan" ;
+	asientos[2][3] = "Yair" ;
+	asientos[2][4] = "David" ;
+	asientos[3][0] = "Rosa" ;
+	asientos[3][1] = "Gael" ;
+	asientos[3][2] = "Violeta" ;
+	asientos[3][3] = "Martha" ;
+	asientos[3][4] = "Yuli" ;
+
+// Muestra la forma en que se encuentran los alumnos en el salón
+
+
+// Define un arreglo que representa la fila de alumnos
+
+// Forma a los alumnos en la fila, primero todos los de
+// la primera columna, luego la segunda columna, etc.
+
+// Muestra la fila de alumnos.
+
+    } // Fin de main
+
+} // Fin clase Salon
